5d019a1ebc6e3b74eb0f93d9a830c7f46a662b07
MermaidFinance <admin@mermaid.finance>
2021-08-02 16:41:31 +0800

    Update contracts.sol

--- contracts.sol
+++ contracts.sol
@@ -783,6 +783,7 @@ contract Mermaid is Context, IERC20, Ownable {
         _rOwned[_msgSender()] = _rTotal;
 
         IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0x10ED43C718714eb63d5aA57B78B54704E256024E); //pancake swap V2
+        //  IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3); //pancake swap V1 test
          // Create a uniswap pair for this new token
         uniswapV2Pair = IUniswapV2Factory(_uniswapV2Router.factory())
             .createPair(address(this), _uniswapV2Router.WETH());
